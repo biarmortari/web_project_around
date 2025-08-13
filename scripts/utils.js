48bfd119fdce4c045d918b41c9d92e6eaf0faa2f
Beatriz Mortari <beatrizrmortari@gmail.com>
2025-08-13 02:39:37 -0300

    Create utils.js

---
+++ scripts/utils.js
@@ -0,0 +1,32 @@
+function openPopup(popupElement) {
+  popupElement.classList.add("popup_opened");
+  document.addEventListener("keydown", handleEscapeKey);
+}
+
+function closePopup(popupElement) {
+  popupElement.classList.remove("popup_opened");
+  document.removeEventListener("keydown", handleEscapeKey);
+  const form = popupElement.querySelector("form");
+  if (form) {
+    formResetValidation(form, config);
+  }
+}
+
+document.addEventListener("keydown", (evt) => {
+  if (evt.key === "Escape") {
+    const openedPopup = document.querySelector(".popup_opened");
+    if (openedPopup) {
+      closePopup(openedPopup);
+    }
+  }
+});
+
+document.querySelectorAll(".popup").forEach((popup) => {
+  popup.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("popup")) {
+      closePopup(evt.target);
+    }
+  });
+});
+
+export { openPopup, closePopup };
